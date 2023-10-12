37a82de6c86ce515789f19d56a4b87632fce6d11
WilliamsNimi <williamson.nimi@gmail.com>
2023-10-12 11:43:03 +0100

    Code Review for console.py
    Added the following functions
    1. do_quit
    2. do_EOF
    3. help_quit
    4. help_EOF
    5. do_create
    6. help_create
    7. do_show
    8. help_show
    9. do_destroy
    10. help_destroy
    
    TODO: Review all codes.

--- console.py
+++ console.py
@@ -3,7 +3,9 @@
 Contains the entry point of the command interpreter
 """
 import cmd
-
+import sys
+from models.base_model import BaseModel
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -17,21 +19,91 @@ class HBNBCommand(cmd.Cmd):
         """Quit command to exit the program
 
         """
-        return True
+        sys.exit(1)
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """
         Command to handle EOF (Ctrl+D or Ctrl+Z) by exiting the console/program
         """
+        sys.exit(1)
+
+    def help_quit(self):
+        """The help function for Quit """
+        print('Quit command to exit the program')
         print("")
-        return True
+
+    def help_EOF(self):
+        """ This is the help function for EOF """
+        print("EOF command to exit the program")
 
     def emptyline(self):
         """
         Executes no command when no input is entered
         """
         pass
+    
+    def do_create(self, arg):
+        """ this is the create command """
+        if arg != "":
+            if arg == "BaseModel":
+                new_class = BaseModel()
+                storage.save()
+                print(new_class.id)
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
 
+    def help_create(self):
+        """ This is the help function for create """
+        print("create class name")
+
+    def do_show(self, arg):
+        """ this is the show command """
+        args = arg.split()
+        print(args[0])
+        if args[0] != "":
+            if args[0] == "BaseModel":
+                new_class = BaseModel()
+                if args[1] != "":
+                    if args[1] == new_class.id:
+                        print(new_class.__repr__)
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def help_show(self):
+        """ This is the help doc for the show command """
+        print("show <classname> <instance-id>")
+
+    def do_destroy(self, arg):
+        """ this is the destroy command """
+        args = arg.split()
+        print(args[0])
+        if args[0] != "":
+            if args[0] == "BaseModel":
+                new_class = BaseModel()
+                if args[1] != "":
+                    if args[1] == new_class.id:
+                        del new_class
+                        storage.save()
+                    else:
+                        print("** no instance found **")
+                else:
+                    print("** instance id missing **")
+            else:
+                print("** class doesn't exist **")
+        else:
+            print("** class name missing **")
+
+    def help_destroy(self):
+        """ This is the help doc for the destroy command """
+        print("destroy <classname> <instance-id>")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
