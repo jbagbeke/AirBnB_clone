07bff576514ee50c187be0d05e7ffd04159abb33
WilliamsNimi <williamson.nimi@gmail.com>
2023-10-15 18:14:56 +0100

    Updated FileStorage TestCases
    Updated the test cases for File Storage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -11,20 +11,43 @@ class TestFileStorage(unittest.TestCase):
         """ The setUp function """
         self.files = FileStorage()
 
-    def tearDown(self):
-        """ The tearDown function """
-        pass
+    def test_file_path_attribute(self):
+        """This function tests the that the FileStorage class has the __file_path attribute"""
+        self.assertTrue(hasattr(self.files, "_FileStorage__file_path"))
+        self.assertFalse(hasattr(self.files, "__file_path"))
+        self.assertIsInstance(self.files.__file_path, str)
+        self.assertEqual(self.files, "")
 
-    def test___init__(self):
-        """ This is the test for the object initialization function """
-        self.assertTrue(hasattr(self.files, "__file_path"))
-        self.assertTrue(hasattr(self.files, "__objects"))
-        self.assertEquals(type(self.files.__filepath), str)
-        self.assertEquals(type(self.files.__objects), dict)
+    def test_objects_attribute(self):
+        """This function tests the that the FileStorage class has the __objects attribute"""
+        self.assertTrue(hasattr(self.files, "_FileStorage__objects"))
+        self.assertFalse(hasattr(self.files, "__objects"))
+        self.assertIsInstance(self.files.__objects, str)
+        self.assertEqual(self.files.__objects, {})
 
     def test_all(self):
-        """ The test for the all function """
-        self.assertEquals(type(self.files.all()), dict)
+        """This function tests the that the FileStorage class has the all function"""
+        self.assertTrue(hasattr(self.files, "all"))
+        self.assertTrue(callable(self.files.all))
+
+    def test_new(self):
+        """This function tests the that the FileStorage class has the new function"""
+        self.assertTrue(hasattr(self.files, "new"))
+        self.assertTrue(callable(self.files.new))
+
+    def test_save(self):
+        """This function tests the that the FileStorage class has the save function"""
+        self.assertTrue(hasattr(self.files, "save"))
+        self.assertTrue(callable(self.files.save))
+
+    def test_reload(self):
+        """This function tests the that the FileStorage class has the reload function"""
+        self.assertTrue(hasattr(self.files, "reload"))
+        self.assertTrue(callable(self.files.reload))
+
+    def tearDown(self):
+        """ The tearDown function """
+        del self.files
 
 
 if __name__ == "__main__":
