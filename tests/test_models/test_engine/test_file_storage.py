b6117a3466409457eebdd1857dbc7ae43221c8ef
WilliamsNimi <williamson.nimi@gmail.com>
2023-10-17 22:35:31 +0100

    Test Cases Fix
    Updated The FileStorage Test Cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -14,25 +14,36 @@ class TestFileStorage(unittest.TestCase):
     def test_file_path_attribute(self):
         """Tests that the FileStorage class has the __file_path attribute"""
         self.assertFalse(hasattr(self.files, "__file_path"))
+        self.assertIsInstance(self.files.__file_path, str, "__file_path should be of type str")
 
     def test_objects_attribute(self):
         """Tests that the FileStorage class has the __objects attribute"""
         self.assertFalse(hasattr(self.files, "__objects"))
+        self.assertIsInstance(self.files.__objects, dict, "__objects should be of type dict")
 
     def test_all(self):
         """Tests that the FileStorage class has the all function"""
         self.assertTrue(hasattr(self.files, "all"))
         self.assertTrue(callable(self.files.all))
+        self.assertEqual(self.files.all(), self.files.__objects)
 
     def test_new(self):
         """Tests that the FileStorage class has the new function"""
         self.assertTrue(hasattr(self.files, "new"))
         self.assertTrue(callable(self.files.new))
+        with self.assertRaises(TypeError) as e:
+            self.files.new(self, 45)
+        message = "new() takes 2 positional argument but 3 were given"
+        self.assertEqual(str(e.exception), message)
 
     def test_save(self):
         """Tests that the FileStorage class has the save function"""
         self.assertTrue(hasattr(self.files, "save"))
         self.assertTrue(callable(self.files.save))
+        with self.assertRaises(TypeError) as e:
+            self.files.save(50)
+        message = "save() takes 1 positional argument but 2 were given"
+        self.assertEqual(str(e.exception), message)
 
     def test_reload(self):
         """Tests that the FileStorage class has the reload function"""
